eb8a5266b014f31eb16d3419b74ed1579230c72c
mikeg64 <m.griffiths@sheffield.ac.uk>
2014-12-19 08:25:07 +0000

    Energy per unit area computed using flux relation quoted by T J Bogdan et al ., 2003, ApjJ, 599, 626

--- matlab/testplot.m
+++ matlab/testplot.m
@@ -5,13 +5,17 @@
 imfile=[ndirectory,'energyvtime_',id,nextension];
 
 
-surf(edifarray(1:1612,38:124)','LineStyle','none');
+%surf(edifarray(1:900,38:124)','LineStyle','none');
+surf(efluxarray(1:634,38:124)','LineStyle','none');
 hold on
 hc=colorbar();
 %caxis([0.5*10^4 -0.5*10^4]);
 %set(hc,'Zlim',[0.5*10^4 -0.5*10^4]);
-caxis([0 0.003]);
-set(hc,'Zlim',[0 0.003]);
+%caxis([0 0.003]);
+%set(hc,'Zlim',[0 0.003]);
+
+caxis([0 1e3]);
+set(hc,'Zlim',[0 1e3]);
 
 set(gca,'CameraPosition',[400 45 17320.508]);
 %set(gca,'YTickLabel',{'0.09';'0.99';'1.94';'2.88';'3.83';'4.77';'5.72';'6.67'})
