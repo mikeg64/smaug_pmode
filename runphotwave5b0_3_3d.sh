a0cc9b7b98c13cfd9556dade7fc393be4ec8741d
mikeg64 <m.griffiths@sheffield.ac.uk>
2015-02-17 09:15:06 +0000

    Running 0,3 mode for 180s and 300s driver with amplitude 147.1 m/s

--- runphotwave5b0_3_3d.sh
+++ runphotwave5b0_3_3d.sh
@@ -2,15 +2,18 @@
 #$ -j y
 ##$ -V 
 #$ -l arch=intel*
-#$ -l gpu=1
+##$ -l gpu=1
+#$ -l gpu=1,gpu_arch=nvidia-k40m
+##$ -l gpu=1,gpu_arch=nvidia-m2070
+
 ##$ -P cs-test
 #$ -P gpu
 #$ -N p5b0p3_3d
 #$ -l mem=12G
 #$ -l rmem=12G
 #$ -l h_rt=168:00:00
-module add libs/cuda/4.0.17
-
+#module add libs/cuda/4.0.17
+module add libs/cuda/6.5.14
 
 cd include
 cp iosmaugparams5b0_3_3d.h iosmaugparams.h
@@ -20,7 +23,7 @@ cd src
 cp usersource5b0_3_3d.cu usersource.cu
 cp boundary_3d.cu boundary.cu
 make clean
-make -f Makefile_3d smaug
+make -f Makefile_3d_k40 smaug
 cd ..
 
 
