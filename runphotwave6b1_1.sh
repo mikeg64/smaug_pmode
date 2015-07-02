50e36a7d65798f113125236969eead67761dbbb0
mikeg64 <m.griffiths@sheffield.ac.uk>
2015-07-02 07:57:39 +0100

    Configuration files for (1,1) modes 180s and 300s driver with amplitude 312.5m/s

--- runphotwave6b1_1.sh
+++ runphotwave6b1_1.sh
@@ -5,7 +5,7 @@
 ##$ -l gpu_arch=nvidia-m2070
 #$ -l gpu=1,gpu_arch=nvidia-k40m
 #$ -P gpu
-#$ -N rep_p6b0p1_3d
+#$ -N p6b1_1
 #$ -l mem=12G
 #$ -l rmem=12G
 #$ -l h_rt=168:00:00
@@ -14,11 +14,11 @@ module load libs/cuda/6.5.14
 
 
 cd include
-cp iosmaugparams6b0_1_3d.h iosmaugparams.h
+cp iosmaugparams6b1_1.h iosmaugparams.h
 cd ..
 
 cd src
-cp usersource6b0_1_3d.cu usersource.cu
+cp usersource6b1_1.cu usersource.cu
 cp boundary_3d.cu boundary.cu
 make clean
 make -f Makefile_3d_k40 smaug
