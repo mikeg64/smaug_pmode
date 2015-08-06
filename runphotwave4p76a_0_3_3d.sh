3eb767794f182348865969b1a15e17ee1fcdc87f
mikeg64 <m.griffiths@sheffield.ac.uk>
2015-08-06 21:23:44 +0100

    Model inputs for 4.2km/s phase curve cs=4.2km/s (0,0) driver 1346.87s (0,1) driver 952.38s (0,2) driver 634.92s (0,3) driver 476.19s

---
+++ runphotwave4p76a_0_3_3d.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#$ -j y
+#$ -V 
+#$ -l arch=intel*
+##$ -l gpu=1,gpu_arch=nvidia-k40m
+#$ -l gpu=1,gpu_arch=nvidia-m2070
+##$ -P cs-test
+#$ -P mhd
+#$ -N j4p76a_0_3
+#$ -l mem=12G
+#$ -l rmem=12G
+#$ -l h_rt=168:00:00
+##module add libs/cuda/4.0.17
+module add libs/cuda/6.5.14
+
+
+#cp solarmodels/Makefile.spicule src/Makefile
+#cp solarmodels/make_inputs.spicule src/make_inputs
+#cp solarmodels/iosac2.5d.cpp.spicule src/iosac2.5d.cpp
+
+#cp solarmodels/boundary.cu.spicule src/boundary.cu
+#cp solarmodels/init_user.cu.spicule src/init_user.cu
+
+#cp solarmodels/usersource.cu.spicule4 src/usersource.cu
+#cp solarmodels/iosmaugparams.h.spicule4 include/iosmaugparams.h
+
+#cd src
+#make clean
+#make smaug
+#cd ..
+
+cd include
+cp iosmaugparams4p76a_0_3_3d.h iosmaugparams.h
+cd ..
+
+cd src
+cp usersource4p76a_0_3_3d.cu usersource.cu
+cp boundary_3d.cu boundary.cu
+make clean
+make -f Makefile_3d smaug
+cd ..
+
+
+
+export TIMECOUNTER=0
+source timeused
+./smaug a
+source timeused
+
