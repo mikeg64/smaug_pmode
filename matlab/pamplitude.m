d848a7d08d44a835eb779c684f837c7ac74602a9
mikeg64 <m.griffiths@sheffield.ac.uk>
2015-12-14 20:10:09 +0000

    Amplitude relation was updated (a factor of 2 was missing). The amplitudes in the source terms have been corrected.

--- matlab/pamplitude.m
+++ matlab/pamplitude.m
@@ -60,19 +60,19 @@
 % <tr>
 %     <td>(1,1)</td> 
 %     <td>471.4</td>
-%     <td>57.75</td>
+%     <td>171.5</td>
 %   <td>spic4p71a_1_1_3d</td>
 %   </tr>
 % <tr>
 %     <td>(2,2)</td> 
 %     <td>235.7</td>
-%     <td>51.39</td>
+%     <td>114.3</td>
 %   <td>spic2p36_2_2_3d</td>
 %   </tr>
 % <tr>
 %     <td>(3,3)</td> 
 %     <td>157.1</td>
-%     <td>43.41</td>
+%     <td>85.8</td>
 %   <td>spic1p57a_3_3_3d</td>
 %   </tr>
 % </tbody></table>
@@ -98,25 +98,25 @@
 % <tr>
 %     <td>(0,0)</td> 
 %     <td>30.0</td>
-%     <td>3683.05</td>
+%     <td>343.4</td>
 %     <td>spicule4b0_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,1)</td> 
 %     <td>30.0</td>
-%     <td>1473.2</td>
+%     <td>217.2</td>
 %     <td>spic4b0_1_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,2)</td> 
 %     <td>30.0</td>
-%     <td>736.61</td>
+%     <td>153.6</td>
 %     <td>spic4b0_2_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,3)</td> 
 %     <td>30.0</td>
-%     <td>433.3</td>
+%     <td>117.8</td>
 %     <td>spic4b0_3_3d</td>
 %   </tr>
 % </tbody></table>
@@ -138,25 +138,25 @@
 % <tr>
 %     <td>(0,0)</td> 
 %     <td>180.0</td>
-%     <td>605.29</td>
+%     <td>348.3</td>
 %     <td>spic6b0_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,1)</td> 
 %     <td>180.0</td>
-%     <td>242.116</td>
+%     <td>220.3</td>
 %     <td>spic6b0_1_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,2)</td> 
 %     <td>180.0</td>
-%     <td>121.06</td>
+%     <td>155.8</td>
 %     <td>spic6b0_2_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,3)</td> 
 %     <td>180.0</td>
-%     <td>71.21</td>
+%     <td>119.5</td>
 %     <td>spic6b0_3_3d</td>
 %   </tr>
 % </tbody></table>
@@ -178,25 +178,25 @@
 % <tr>
 %     <td>(0,0)</td> 
 %     <td>300.0</td>
-%     <td>362.7712</td>
+%     <td>350</td>
 %     <td>spic5b0_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,1)</td> 
 %     <td>300.0</td>
-%     <td>145.1085</td>
+%     <td>221.4</td>
 %     <td>spic5b0_1_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,2)</td> 
 %     <td>300.0</td>
-%     <td>75.5542</td>
+%     <td>156.5</td>
 %     <td>spic5b0_2_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,3)</td> 
 %     <td>300.0</td>
-%     <td>42.679</td>
+%     <td>120.0</td>
 %     <td>spic5b0_3_3d</td>
 %   </tr>
 % </tbody></table>
@@ -221,25 +221,25 @@
 % <tr>
 %     <td>(0,0)</td> 
 %     <td>282.84</td>
-%     <td>384.82</td>
+%     <td>350.3</td>
 %    <td>spic2p82a_0_0_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,1)</td> 
 %     <td>200.00</td>
-%     <td>212.84</td>
+%     <td>213.5</td>
 %   <td>spic2p00a_0_1_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,2)</td> 
 %     <td>133.33</td>
-%     <td>163.59</td>
+%     <td>154.1</td>
 %   <td>spic1p33a_0_2_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,3)</td> 
 %     <td>100.00</td>
-%     <td>128.47</td>
+%     <td>118.1</td>
 %   <td>spic1p00a_0_3_3d</td>
 %   </tr>
 % </tbody></table>
@@ -248,9 +248,9 @@
 
 
 % </html>
-
+%below need correcting
 %% %% Table of amplitudes for normal mode
-% Sound speed is 31.43km/s
+% Sound speed is 31.43km/s%below need correcting
 
 %%
 % 
@@ -266,25 +266,25 @@
 % <tr>
 %     <td>(0,0)</td> 
 %     <td>179.98</td>
-%     <td>605.3599</td>
+%     <td>348.2</td>
 %    <td>spic2p82a_0_0_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,1)</td> 
 %     <td>127.27</td>
-%     <td>342.8256</td>
+%     <td>345</td>
 %   <td>spic2p00a_0_1_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,2)</td> 
 %     <td>84.84</td>
-%     <td>257.6476</td>
+%     <td>344.6</td>
 %   <td>spic1p33a_0_2_3d</td>
 %   </tr>
 % <tr>
 %     <td>(0,3)</td> 
 %     <td>63.63</td>
-%     <td>202.4767</td>
+%     <td>341.3</td>
 %   <td>spic1p00a_0_3_3d</td>
 %   </tr>
 % </tbody></table>
@@ -297,7 +297,7 @@ function amplitude=pamplitude(n,m,t00,tnm,a00,tm)
     amplitude2=((a00.^2)./(n.^2+m.^2+2.*(n+m)+2)) ;
     t1=tm-(t00/(4*pi)).*sin(4*pi*tm/t00);
     t2=tm-(tnm/(4*pi)).*sin(4*pi*tm/tnm);
-    amplitude=sqrt(amplitude2*t1/t2);
+    amplitude=sqrt(2*amplitude2*t1/t2);
 
 
 
